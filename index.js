770b1d2964126dbd746c566eccb8ced628ae5ae9
허제인 <codingjamee@gmail.com>
2024-06-02 12:39:17 +0900

    수정

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ function statement(invoice, plays) {
     currency: "USD",
     minimumFractionDigits: 2,
   }).format;
-
   for (let perf of invoice.performances) {
     const play = plays[perf.playID];
     let thisAmount = 0;
@@ -35,10 +34,22 @@ function statement(invoice, plays) {
     //포인트를 적립한다.
     volumeCredits += Math.max(perf.audience - 30, 0);
     //희극 관객 5명마다 추가 포인트를 제공한다.
-    if ("comedy" === perf.type) volumeCredits += Math.floor(perf.audience / 5);
+    if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
     //청구 내역을 출력한다.
-    result += `총액: ${usd(totalAmount())}\n`;
-    result += `적립 포인트 : ${volumeCredits}점\n`;
-    return result;
+    result += ` ${play.name}: ${format(thisAmount / 100)} (${
+      perf.audience
+    }석)\n`;
+    totalAmount += thisAmount;
   }
+  result += `총액: ${format(totalAmount / 100)}\n`;
+  result += `적립 포인트 : ${volumeCredits}점\n`;
+  return result;
 }
+
+//결과 화면 
+//청구내역 (고객명 : BigCo)
+//Hamlet : $650.00(55석)
+//As You Like It: $580.00(35석)
+//Othello: %500.00(40석)
+//총액: $1,730.00
+//적립포인트: 47점
